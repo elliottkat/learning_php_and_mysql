5adf50beaeb24b747e4d4c4e0c4381526cba34e7
Katherine Elliott <katherine.elliott@hpe.com>
2023-12-30 11:20:51 -0700

    Prettified the table

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ function listMovies($conn) {
     $result = mysqli_query($conn, $get_sql);
     if (mysqli_num_rows($result) > 0) {
         echo "<strong><br>MOVIE LIST:</strong><br><br>";
-        echo "<table border=2 class='styled-table'><tr><th>Movie Name</th><th>Genre</th><th>Lead Studio</th><th>Rating</th><th>Year</th></tr>";
+        echo "<table border=2 width='35%'><tr align='center' bgcolor='#98fb98'><th height='50px'>Movie Name</th><th>Genre</th><th>Lead Studio</th><th>Rating</th><th>Year</th></tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr class='active-row'><td>" . $row["MovieName"] . "</td><td>" . $row["Genre"] . "</td><td>" . $row["LeadStudio"] . "</td><td>" . $row["AudienceRating"] . "</td><td>" . $row["ReleaseYear"] . "</td></tr>";
+            echo "<tr class='active-row'><td height='40px'>" . $row["MovieName"] . "</td><td>" . $row["Genre"] . "</td><td>" . $row["LeadStudio"] . "</td><td>" . $row["AudienceRating"] . "</td><td>" . $row["ReleaseYear"] . "</td></tr>";
         }
         echo "</table>";
     } else {
